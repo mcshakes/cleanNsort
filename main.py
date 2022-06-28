f9b4fa00ed50b9746bdd7dc4e7cf00241e553800
mcshakes <efmccarthy26@gmail.com>
2022-06-28 11:15:53 -0600

    filetypes and ignore

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
 import getpass
+import filetype
+
 # get all extensions in directory
 current_dir = os.getcwd()
 
 # get current user
 current_user = getpass.getuser()
-
-
-
 
 
 def navigate_to(dir: str):
@@ -20,3 +19,15 @@ def check_extensions():
 
 navigate_to("Desktop")
 check_extensions()
+
+def main():
+  pass
+
+if __name__ == '__main__':
+    main()
+# go to a location
+# instead of checking extensions first, 
+# Iterate through all the files in the location, if there is an extension we know of
+  #  create the folder and put the file in there
+
+
